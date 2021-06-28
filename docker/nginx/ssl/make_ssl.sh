c37c49fd3040773024c1b10b17cfcf801ad8fb36
mannv <mannv@vnext.com.vn>
2021-06-28 13:05:15 +0700

    first commit

---
+++ docker/nginx/ssl/make_ssl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+openssl genrsa -out "/etc/nginx/ssl/default.key" 2048
+openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
+openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
---
+++ src/.bash_history
@@ -0,0 +1,14 @@
+pwd
+ls -all
+cd public/
+ls -all
+cd ..
+cd /usr/local/etc/php/conf.d
+ls -all
+cat laravel.ini 
+cd ..
+ls -all
+cat php.ini
+ls -all
+ls -all
+pwd
